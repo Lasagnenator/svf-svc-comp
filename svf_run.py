cfd3a27b5dbc8d894f25a2df40aea8940e816478
Lasagnenator <38394321+Lasagnenator@users.noreply.github.com>
2024-10-16 10:44:18 +1100

    Not using yaml anymore since i think we aren't meant to see that file.

--- svf_run.py
+++ svf_run.py
@@ -9,12 +9,12 @@ import re
 import sys
 import subprocess
 import tempfile
-import yaml
 
 VERSION = "1.0"
 
 # Generic preprocessor fix.
-INCLUDE_REPLACE = "include_replace.c"
+INCLUDE_REPLACE = os.path.join(os.path.dirname(__file__), "include_replace.c")
+#INCLUDE_REPLACE = "include_replace.c"
 
 # Patterns for replacement.
 # This prevents the #define from making a duplicate.
@@ -25,53 +25,31 @@ def replacement(text: str):
     # replace asserts with SVF's assert.
     return text.replace(svc_assert, svc_assert_replace)
 
-def get_real_path(yaml_file, relative):
-    # Find the real path given the base yaml file and a path relative to the yaml
-    return os.path.join(os.path.dirname(yaml_file), relative)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--version", action="version", version=f"{VERSION}")
+    parser.add_argument("c_file", help="input C file")
+    parser.add_argument("--bits", choices=["32","64"], help="bit width")
+    parser.add_argument("--prop", action="append", help="property files", default=[])
 
-def read_yaml(yaml_file: str):
-    with open(yaml_file, "r") as f:
-        specification =  yaml.safe_load(f.read())
-
-    assert specification["format_version"] == "2.0"
-    assert specification["options"]["language"] == "C"
-
-    if specification["options"]["data_model"] == "ILP32":
-        bits = 32
-    else:
-        bits = 64
+    args, extra = parser.parse_known_args()
+    input_file = args.c_file
+    bits = args.bits
+    properties = args.prop
 
     # TODO: Find some way of making clang actually compile 32 bit
     # binaries on a 64 bit environment without errors.
     # Override bit width since it won't compile in 32 bit mode.
     bits = 64
 
-    c_file = get_real_path(yaml_file, specification["input_files"])
-
-    # Properties is a list of dictionaries
-    properties = []
-    for prop in specification["properties"]:
-        file = prop["property_file"]
-        prop.update({"property_file": get_real_path(yaml_file, file)})
-        properties.append(prop)
-
-    return c_file, bits, properties
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("yaml_file", help="The input yaml file")
-    parser.add_argument("--version", action="version", version=f"{VERSION}")
-
-    args = parser.parse_args()
-
-    input_file, bits, properties = read_yaml(args.yaml_file)
-
     print(f"Running analysis: {input_file}.")
     print(f"Selected bit width: {bits}.")
     print(f"Properties: {properties}.")
+    print(f"Extra (unused) options: {extra}.")
 
     buffer = tempfile.NamedTemporaryFile("w+", suffix=".c")
     with open(INCLUDE_REPLACE, "r") as f:
+        print(f"Using include_replace: {INCLUDE_REPLACE}.")
         buffer.write(f.read())
 
     with open(input_file, "r") as f:
@@ -80,13 +58,16 @@ if __name__ == "__main__":
     # buffer now contains our fixed code to pass into SVF.
     buffer.flush()
 
-    command = ["clang", "-v", "-S", "-emit-llvm", "-o", "working.ll", f"-m{bits}"]
-
+    command = ["clang", "-S", "-emit-llvm", "-o", "working.ll", f"-m{bits}"]
     command.append(buffer.name)
+
+    print(f"Running clang with command: {' '.join(command)}")
     subprocess.run(command)
 
     buffer.close()
 
     # Run SVF on the resulting file.
     # TODO: Get SVF built and ready to use.
-    #subprocess.run(["./bin/svf-ex", "working.ll"])
+    #command = ["./bin/svf-ex", "working.ll"]
+    #print(f"Running SVF with command: {' '.join(command)}")
+    #subprocess.run(command)
