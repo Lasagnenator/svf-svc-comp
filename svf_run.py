f4ca0a5d9a1a44420c632577b27c63094409e33e
Lasagnenator <38394321+Lasagnenator@users.noreply.github.com>
2025-10-21 22:08:13 +1100

    Move new file to original main file's name.

--- svf_run.py
+++ svf_run.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
-# Wrapper around SVF to adapt to SVC
-# SVF: https://github.com/SVF-tools/SVF
-# SVC: https://sv-comp.sosy-lab.org/2025/
+#! /usr/bin/env python3
+
 
 import argparse
+import yaml
 import os
-import re
 import sys
 import subprocess
 import tempfile
@@ -14,8 +12,10 @@ import nondet
 from util import *
 import strategies
 import witness_output
+from AbstractInterpretation import *
 
 def main():
+    # this implementation just accepts the file to do testing on
     parser = argparse.ArgumentParser()
     parser.add_argument("--version", action="version", version=VERSION)
     parser.add_argument("--bits", choices=["32","64"], help="bit width", default="64")
@@ -27,44 +27,60 @@ def main():
 
     args, extra = parser.parse_known_args()
     log(f"Arguments: {args}")
-    log(f"Extra (unused) arguments: {extra}")
 
-    input_file = args.c_file
-    bits = args.bits
-    prop_file = args.prop
-    witness_file = args.witness
+    runSVF(args.c_file, args.prop)
 
-    # TODO: Find some way of making clang actually compile 32 bit
-    # binaries on a 64 bit environment without errors.
-    # Override bit width since it won't compile in 32 bit mode.
-    bits = "64"
 
-    log(f"Running analysis: {input_file}.")
-    log(f"Selected bit width: {bits}.")
-    log(f"Property file: {prop_file}.")
-    log(f"SVF time limit: {args.time_limit}.")
-    log(f"Witness output file: {witness_file}.")
 
+    # Im not sure of the actual input format, implemented it to follow the format for verification
+    # tasks from gitlab.com/sosy-lab/benchmarking/sv-benchmarks/ but that seems to be
+    # different to whats actually used in the competition
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("yamlfile", help="yaml file that defines verification task")
+
+    # args, extra = parser.parse_known_args()
+    # log(f"Arguments: {args}")
+
+    # # Reading verification specification from provided yaml file
+    # with open(args.yamlfile) as stream:
+    #     try:
+    #         yaml_contents = yaml.safe_load(stream)
+    #         input_file_name = yaml_contents["input_files"]
+    #         path_prefix = args.yamlfile[::-1].split("/", 1)[1][::-1]
+    #         input_file_path = path_prefix + "/" + input_file_name
+
+    #         # This creates the paths for the property files (assuming sv-benchmarks is installed)
+    #         property_files = [(x["property_file"]).replace("../", "sv-benchmarks/c/") for x in yaml_contents["properties"]]
+
+
+    #         # Currently attempting to modify our use of SVF itself instead of modifying the code
+
+
+    #     except yaml.YAMLError as exc:
+    #         log(exc)
+
+
+# Accepts a C source file, and traverses its ICFG using the SVF framework
+def runSVF(input_file_path, prop_file_path):
+    # Preprocesses the C source file by replacing the nondet function calls
     buffer = tempfile.NamedTemporaryFile("w+", suffix=".c")
-
-    with open(input_file, "r") as f:
+    with open(input_file_path, "r") as f:
         c_code = f.read()
-        buffer.write(nondet.generate_nondet(c_code))
-        strategy = strategies.apply_strategy(c_code, prop_file)
-        replaced, exe, svf_options, category = strategy
-        buffer.write(replaced)
+        c_code = nondet.generate_nondet_replacing(c_code)
 
-    # buffer now contains our fixed code to pass into SVF.
+        buffer.write(c_code)
+
     buffer.flush()
 
-    if args.verbose:
-        log("Generated file:")
-        buffer.seek(0)
-        log(buffer.read())
+    log("Generated file:")
+    buffer.seek(0)
+    log(buffer.read())
 
+    # Compiles the C source file to LLVMIR
     working_file = tempfile.NamedTemporaryFile("w+", suffix=".ll")
 
-    command = ["clang", "-S", "-emit-llvm", "-o", working_file.name, f"-m{bits}"]
+    command = ["clang", "-S", "-c", "-O0", "-fno-discard-value-names", "-g", "-emit-llvm", "-o", working_file.name,]
     command.append(buffer.name)
 
     log(f"Running clang with command: {' '.join(command)}")
@@ -73,32 +89,58 @@ def main():
 
     if retcode != 0:
         log(f"Clang failed to output {working_file.name}. SVF will fail.")
-        fail("ERROR(CLANG)", retcode)
+        log("ERROR(CLANG)")
+        exit(retcode)
 
     buffer.close()
 
-    # Run SVF on the resulting file.
-    svf_bin = get_real_path(f"svf/bin/{exe}")
-    extapi = get_real_path("svf/lib/extapi.bc")
-    command = [f"{svf_bin}", f"-extapi={extapi}"]
-    command.extend(svf_options)
+    # This code is copied from python/test-ae.py to use SVF
+    pysvf.buildSVFModule(working_file.name)
+    pag = pysvf.getPAG()
+    ass3 = AbstractExecution(pag)
+    ass3.analyse()
 
-    if not args.verbose:
-        # Disable very long output from SVF.
-        command.append("-stat=false")
+    # Currently the results are stored as a dictionary in the AbstractExecution class
+    # feel free to change how its stored to be more convenient
+    log(ass3.results)
 
-    if args.time_limit != -1:
-        command.extend(["-clock-type=cpu", f"-fs-time-limit={args.time_limit}"])
 
-    command.append(working_file.name)
+    # parse input prop file path to find the file name
+    prop_file_name = prop_file_path.split('/')[-1]
 
-    log(f"Running SVF with command: {' '.join(command)}")
+    if prop_file_name == 'unreach-call.prp':
+        error_detected = False
+        # currently for the nodes with unreach_call, if they are traversed to from the ICFG traversal,
+        # their feasibility will be added to this part of the results dictionary
+        #
+        # if an unreach_call node is reachable, then it will always be added to the list ass3.results["reach"] = [list of nodes]
+        #
+        # we only care about the reachable nodes, because if they are reachable, there is an error in the C code
+        for (is_feasible, callNode) in ass3.results["reach"]:
+            # if an unreach_call is ever feasible,
+            # then a verifier_assert is provided with a false statement
+            error_detected |= is_feasible
 
-    process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = strategies.interpret_output(process, strategy)
-    print(result)
-    witness_output.generate_witness(result, args, witness_file)
-    working_file.close()
+        if error_detected:
+            print("REACH Incorrect")
+            witness_output.generate_witness_v2("Incorrect", input_file_path, prop_file_path, "witness.xml")
+        else:
+            witness_output.generate_witness_v2("Correct", input_file_path, prop_file_path, "witness.xml")
+    elif prop_file_name == 'no-overflow.prp':
+        # if the list of SVFstmts where buffer overflows occur is non-zero, then there are buffer overflows
+        # (kinda because of how our use of the SVF python API is done)
+        if len(ass3.results["bufferoverflow"]) > 0:
+            # idk if this is the right type of memory error
+            print("OVERFLOW Incorrect")
+            witness_output.generate_witness_v2("Incorrect", input_file_path, prop_file_path, "witness.xml")
+        else:
+            witness_output.generate_witness_v2("Correct", input_file_path, prop_file_path, "witness.xml")
+
+
+    ###TODO: right now it doesnt do witness output, have to implement that soon
+
+    pysvf.releasePAG()
+
 
 if __name__ == "__main__":
     main()
