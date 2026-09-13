a8b620f47cbb191d057d1e8d28d7b935fa0e9ceb
Haoran Cui <haorancuiunsw@gmail.com>
2026-09-13 13:38:59 +1000

    Fix schema violations in format 2.0 witness output

--- generate_witness.py
+++ generate_witness.py
@@ -12,12 +12,12 @@ import yaml
 Sample witness
 - entry_type: invariant_set
   metadata:
-    format_version: 2.0
+    format_version: '2.0'
     uuid: dc8eb57e-7eb3-4c34-9422-460d710e73f5
     creation_time: '2025-07-02T17:34:34.967309'
     producer:
       name: svf-svc
-      version: 2.0
+      version: '2.0'
     task:
       input_file:
       - multivar_1-1.c
@@ -55,14 +55,14 @@ def write_witness(invariants: list, input_files: list, specification: str, outpu
     yaml_list.append(dict())
     yaml_list[-1]['entry_type'] = 'invariant_set'
     yaml_list[-1]['metadata'] = {
-        'format_version': 2.0,
+        'format_version': '2.0',
         'uuid': str(uuid.uuid4()),
         'creation_time': datetime.datetime.now().isoformat()
     }
 
     yaml_list[-1]['metadata']['producer'] = {
         'name': 'svf-svc',
-        'version': 2.0  # Update based on svc-svf version
+        'version': '2.0'  # Update based on svc-svf version
         #'configuration': '(Optional)', The configuration in which the tool ran.
         #'command_line': '(Optional)', The command line with which the tool ran;
         #'description': '(Optional)' Any information not fitting in the previous items.
@@ -70,7 +70,7 @@ def write_witness(invariants: list, input_files: list, specification: str, outpu
     }
 
     yaml_list[-1]['metadata']['task'] = {
-        'input_file': input_files,
+        'input_files': input_files,
         'input_file_hashes': hashes,
         'specification': specification,
         'data_model': 'ILP32', # can be ILP32 or LP64
@@ -106,7 +106,7 @@ if __name__ == "__main__":
       'type': 'loop_invariant',
       'file_name': 'multivar_1-1.c',
       'line': 22,
-      'column': 0, # Set to None if not provided
+      'column': None, # Set to None if not provided
       'function': 'main', # Set to None if not provided
       'value': '( y == x )',
     }]
