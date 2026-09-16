99ba80c7c6145154753dcb83e78170c05b232005
Haoran Cui <haorancuiunsw@gmail.com>
2026-09-16 11:16:10 +1000

    mark the limit of current witness generation

--- generate_witness.py
+++ generate_witness.py
@@ -73,6 +73,7 @@ def write_witness(invariants: list, input_files: list, specification: str, outpu
         'input_files': input_files,
         'input_file_hashes': hashes,
         'specification': specification,
+        # TODO: take from --bits once the entrypoint honours it
         'data_model': 'ILP32', # can be ILP32 or LP64
         'language': 'C'
     }
